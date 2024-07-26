88066e3420597b93455e68ee108ac3286c4d9b95
Utkarsh Gupta <utkarshpengoriya@gmail.com>
2024-07-26 23:30:37 +0530

    update routing

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,8 @@
 import Head from 'next/head'
-import Complaint from './components/Complaint'
-import Admin from './components/Admin'
 import Header from './components/Header'
-import Status from './components/Status'
-import { useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
+
 
 export default function Home() {
-  const address = useAddress();
-
-  const { contract } = useContract(process.env.NEXT_PUBLIC_SMART_CONTRACT);
-  const { data: officer } = useContractRead(contract, "officer")
-
   return (
     <div className="" >
       <Head>
@@ -19,11 +11,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <Complaint />
-      <Status />
-      {officer === address && (
-        <Admin />
-      )}
     </div>
   )
 }
